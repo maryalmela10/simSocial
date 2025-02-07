b9e88850b674a71c9fc40be6fb5c4645dff61adf
pablo <pablodpv9@gmail.com>
2025-02-07 10:14:57 +0100

    qw

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -24,52 +26,61 @@ class UsuarioController extends AbstractController
     {
         $usuarioActual = $this->getUser();
         $todosUsuarios = $this->entityManager->getRepository(Usuario::class)->findAll();
-    
-    $usuarios = array_filter($todosUsuarios, function($usuario) use ($usuarioActual) {
-        return $usuario !== $usuarioActual;
-    });
 
-    return $this->render('inicio.html.twig', [
-        'usuarios' => $usuarios,
-    ]);
+        $usuarios = array_filter($todosUsuarios, function ($usuario) use ($usuarioActual) {
+            return $usuario !== $usuarioActual;
+        });
+
+        return $this->render('inicio.html.twig', [
+            'usuarios' => $usuarios,
+        ]);
     }
 
-    // #[Route('/usuarios', name: 'listar_usuarios')]
-    // public function listarUsuarios(): Response
-    // {
-    //     $usuarioActual = $this->getUser();
-    //     $todosUsuarios = $this->entityManager->getRepository(Usuario::class)->findAll();
-    
-    // $usuarios = array_filter($todosUsuarios, function($usuario) use ($usuarioActual) {
-    //     return $usuario !== $usuarioActual;
-    // });
-
-    // return $this->render('usuarios.html.twig', [
-    //     'usuarios' => $usuarios,
-    //     ]);
-    // }
-
     #[Route('/usuario/{id}', name: 'ver_perfil')]
-    public function verPerfil(EntityManagerInterface $entityManager, int $id) 
+    public function verPerfil(EntityManagerInterface $entityManager, int $id)
     {
-         $usuarioActual = $this->getUser();
+        $usuarioActual = $this->getUser();
 
-         if ($usuarioActual instanceof Usuario && $usuarioActual->getId() == $id) {
+        if ($usuarioActual instanceof Usuario && $usuarioActual->getId() == $id) {
             // Redirige al controlador miPerfil
             return $this->redirectToRoute('miPerfil', ['id_usuario' => $id]);
         }
 
-         $usuario = $entityManager->getRepository(Usuario::class)->find($id);
-         
-         if (!$usuario) {
-             throw $this->createNotFoundException('Usuario no encontrado');
-         }
- 
-         $posts = $usuario->getPosts();
- 
-         return $this->render("perfilOtro.html.twig", [
-             'posts' => $posts,
-             'usuario' => $usuario
-         ]);
+        $usuario = $entityManager->getRepository(Usuario::class)->find($id);
+
+        if (!$usuario) {
+            throw $this->createNotFoundException('Usuario no encontrado');
+        }
+
+        $posts = $usuario->getPosts();
+
+        return $this->render("perfilOtro.html.twig", [
+            'posts' => $posts,
+            'usuario' => $usuario
+        ]);
     }
+
+    #[Route('/buscar-usuarios', name: 'buscar_usuarios', methods: ['POST'])]
+    public function buscarUsuarios(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $query = $data['q'] ?? '';
+
+        $usuarios = $this->entityManager->getRepository(Usuario::class)->createQueryBuilder('u')
+            ->where('u.nombre LIKE :query OR u.email LIKE :query')
+            ->setParameter('query', '%' . $query . '%')
+            ->getQuery()
+            ->getResult();
+
+        $resultados = array_map(function ($usuario) {
+            return [
+                'id' => $usuario->getId(),
+                'nombre' => $usuario->getNombre(),
+                'email' => $usuario->getEmail()
+            ];
+        }, $usuarios);
+
+        return new JsonResponse($resultados);
+    }
+
 }
