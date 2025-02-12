561cdf8764ab9346af2375675b24e9510f15e85d
pablo <pablodpv9@gmail.com>
2025-02-12 09:08:16 +0100

    aq

--- src/Controller/PedidosBase.php
+++ src/Controller/PedidosBase.php
@@ -26,12 +26,29 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 #[IsGranted('ROLE_USER')]
 class PedidosBase extends AbstractController
 {
-	#[Route('/inicio', name:'inicio')]
-    public function mostrarInicio(EntityManagerInterface $entityManager) {
-        // $categorias = $entityManager->getRepository(Categoria::class)->findAll();
-        // return $this->render("categorias.html.twig", ['categorias'=>$categorias]);
-        return $this->render("inicio.html.twig");
+    #[Route('/inicio', name: 'inicio')]
+    public function mostrarInicio(EntityManagerInterface $entityManager)
+    {
+        $usuarioActual = $this->getUser();
+
+        // Obtener todos los posts, ordenados por fecha de publicación descendente
+        $posts = $entityManager->getRepository(Post::class)
+            ->findBy([], ['fecha_publicacion' => 'DESC'], 10);
+
+        $usuarios = $entityManager->getRepository(Usuario::class)
+            ->createQueryBuilder('u')
+            ->where('u != :usuarioActual')
+            ->setParameter('usuarioActual', $usuarioActual)
+            ->getQuery()
+            ->getResult();
+        dump($posts);
+
+        return $this->render("inicio.html.twig", [
+            'posts' => $posts,
+            'usuarios' => $usuarios
+        ]);
     }
+
 
     #[Route('/miPerfil/{id_usuario}', name: 'miPerfil')]
     public function miPerfil(EntityManagerInterface $entityManager, Security $security, $id_usuario, Request $request)
